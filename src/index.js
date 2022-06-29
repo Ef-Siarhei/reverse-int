7633c1114d203aba2a8cce3aff4af5ad9c6a6b1f
Siarhei <h1_n1@mail.ru>
2022-06-29 14:36:08 +0300

    feat: add function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    str = String(n);
+    mus = str.split("");
+    musReverse = mus.reverse();
+    return Number(musReverse.join(''));
 }
